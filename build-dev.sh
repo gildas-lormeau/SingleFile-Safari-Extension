42901dcb7c2d849cc0fb175ee0f8dc0c8d12a5f9
Gildas <gildas.lormeau@gmail.com>
2022-11-14 20:44:42 +0100

    fix script

--- build-dev.sh
+++ build-dev.sh
@@ -4,8 +4,8 @@ cd ./single-file
 npx rollup -c rollup.config.dev.js
 cd ..
 
-rm -rf ./SingleFile\ Safari\ Extension/Resources/*
-cp -R ./single-file/_locales ./SingleFile\ Safari\ Extension/Resources
-cp -R ./single-file/lib ./SingleFile\ Safari\ Extension/Resources
-cp -R ./single-file/src ./SingleFile\ Safari\ Extension/Resources
-cp ./single-file/manifest.json ./SingleFile\ Safari\ Extension/Resources/
+rm -rf ./Shared\ \(Extension\)/Resources/*
+cp -R ./single-file/_locales ./Shared\ \(Extension\)/Resources
+cp -R ./single-file/lib ./Shared\ \(Extension\)/Resources
+cp -R ./single-file/src ./Shared\ \(Extension\)/Resources
+cp ./single-file/manifest.json ./Shared\ \(Extension\)/Resources/
