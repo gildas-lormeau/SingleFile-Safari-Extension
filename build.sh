46bca4a5f940bdc31cc6e299a037430074d325e7
Gildas <gildas.lormeau@gmail.com>
2024-08-01 23:39:04 +0200

    fix script

--- build.sh
+++ build.sh
@@ -11,5 +11,6 @@ cp -R ./single-file/src ./Shared\ \(Extension\)/Resources
 cp ./single-file/manifest.json ./Shared\ \(Extension\)/Resources/
 
 jq "del(.optional_permissions,.incognito,.permissions[0],.permissions[2])" ./Shared\ \(Extension\)/Resources/manifest.json > ./Shared\ \(Extension\)/Resources/manifest.tmp.json
-sed -i "" 's/menus/contextMenus/g' manifest.tmp.json
+sed -i "" 's/menus/contextMenus/g' ./Shared\ \(Extension\)/Resources/manifest.tmp.json
+sed -i "" 's/background.html"/background.html", "persistent": false/g' ./Shared\ \(Extension\)/Resources/manifest.tmp.json
 mv ./Shared\ \(Extension\)/Resources/manifest.tmp.json ./Shared\ \(Extension\)/Resources/manifest.json
