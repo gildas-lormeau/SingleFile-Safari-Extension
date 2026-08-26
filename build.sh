6bdf93f07e6cef5001275dbf82358bc3caa63af4
Gildas <gildas.lormeau@gmail.com>
2026-08-26 17:24:24 +0200

    inject the Woleet API key at build time in Xcode

--- build.sh
+++ build.sh
@@ -10,6 +10,14 @@ cp -R ./single-file/lib ./Shared\ \(Extension\)/Resources
 cp -R ./single-file/src ./Shared\ \(Extension\)/Resources
 cp ./single-file/manifest.json ./Shared\ \(Extension\)/Resources/
 
+# The committed resources must stay keyless, the Woleet API key is injected
+# into the built app by the "Inject Woleet API Key" Xcode build phase
+if ! grep -q "WOLEET_API_KEY_PLACEHOLDER" ./Shared\ \(Extension\)/Resources/src/lib/woleet/woleet.js ||
+    grep -q "eyJhbGciOiJIUzI1NiJ9" ./Shared\ \(Extension\)/Resources/src/lib/woleet/woleet.js ./Shared\ \(Extension\)/Resources/lib/single-file-extension-background.js; then
+    echo "The Woleet API key placeholder is missing or a real key was copied into the resources"
+    exit 1
+fi
+
 jq "del(.optional_permissions,.incognito,.permissions[0],.permissions[2])" ./Shared\ \(Extension\)/Resources/manifest.json > ./Shared\ \(Extension\)/Resources/manifest.tmp.json
 sed -i "" 's/menus/contextMenus/g' ./Shared\ \(Extension\)/Resources/manifest.tmp.json
 sed -i "" 's/background.html"/background.html", "persistent": false/g' ./Shared\ \(Extension\)/Resources/manifest.tmp.json
